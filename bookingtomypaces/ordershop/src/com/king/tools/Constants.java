2c8631b07c1cee69b05966390f2cb8cf2916b65c
blueshoes12 <blueshoes12@gmail.com>
2010-07-05 12:42:28 +0000

    bug

--- bookingtomypaces/ordershop/src/com/king/tools/Constants.java
+++ bookingtomypaces/ordershop/src/com/king/tools/Constants.java
@@ -4,7 +4,7 @@ public abstract class  Constants {
 
 	// 保存在session中的FrmUser
 	public static String SESSION_SYSTEM_USER = "KING_SYSTEM_USER";
-	public static int PAGE_SIZE = 15;
+	public static int PAGE_SIZE = 10;
 
 	public static String ProjectName="/ordershop";
 	
