045d86503bd6151e10ee2418a5ac96da57954c78
blueshoes12@gmail.com <blueshoes12@gmail.com@e8f53820-53fd-2bca-8b4b-032b26a4b237>
2010-07-11 06:42:44 +0000

    多數據庫接入

---
+++ trunk/bookingtomypaces/ordershop/src/com/king/web/usermanage/user/dao/OneUserDAO.java
@@ -0,0 +1,34 @@
+
+/**   
+* 文件名：OneUserDAO.java   
+*   
+* 版本信息：   
+* 日期：2010-7-11   
+* Copyright 足下 Corporation 2010    
+* Mypaces 版权所有   
+*   
+*/
+
+package com.king.web.usermanage.user.dao;
+
+import com.king.base.BasePersistenceOne;
+import com.king.web.usermanage.user.data.UserData;
+
+
+/**   
+ *    
+ * 项目名称：ordershop   
+ * 类名称：OneUserDAO   
+ * 类描述：   
+ * 创建人：tim   
+ * 创建时间：2010-7-11 上午02:32:55   
+ * 修改人：tim   
+ * 修改时间：2010-7-11 上午02:32:55   
+ * 修改备注：   
+ * @version    
+ *    
+ */
+
+public class OneUserDAO extends BasePersistenceOne<UserData> implements IOneUserDAO{
+
+}
